de4b4939dba33bfc393cec7f5d9bcd2afc9563a4
Andy Liu <andyliuliming@outlook.com>
2018-11-27 17:28:32 +0800

    kubectl apply part.

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -31,3 +31,15 @@ pushd $DIR/docker-images
   docker push $ACR_NAME.azurecr.io/samples/clustered-app
 popd
 
+
+
+# Get the id of the service principal configured for AKS
+CLIENT_ID=$(az aks show --resource-group $RESOURCE_GROUP_NAME --name $AKS_CLUSTER_NAME --query "servicePrincipalProfile.clientId" --output tsv)
+
+# Get the ACR registry resource id
+ACR_ID=$(az acr show --name $ACR_NAME --resource-group $RESOURCE_GROUP_NAME --query "id" --output tsv)
+
+# Create role assignment
+az role assignment create --assignee $CLIENT_ID --role Reader --scope $ACR_ID
+
+kubectl apply -f ./wildfly-service.yaml
