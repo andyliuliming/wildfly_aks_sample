6e71011de6571673d6f6de21d5214722dbfe2efe
Andy Liu <andyliuliming@outlook.com>
2018-11-27 19:52:41 +0800

    put drain cpu logic in.

--- maven/webapp/src/main/java/com/redhat/demo/clusteredapp/MainServlet.java
+++ maven/webapp/src/main/java/com/redhat/demo/clusteredapp/MainServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Random;
 
 @WebServlet("/")
 public class MainServlet extends HttpServlet {
@@ -18,6 +19,20 @@ public class MainServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
-        response.getWriter().println("Hello, my node name is " + System.getProperty("jboss.node.name") + ", session counter is " + sessionCounter.getAndIncrement());
+                String inputCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\n";
+       int stringLength = 200000;
+      
+       //response.getWriter().println("Hello, my node name is " + System.getProperty("jboss.node.name") + ", session counter is " + sessionCounter.getAndIncrement());
+       response.getWriter().println("Randomly generated string to drive up CPU usage is: " + generateRandomString(inputCharacters, stringLength));
+        // response.getWriter().println("Hello, my node name is " + System.getProperty("jboss.node.name") + ", session counter is " + sessionCounter.getAndIncrement());
+    }
+
+    public static String generateRandomString(String inputCharacters, int length) {
+        StringBuilder stringBuilder = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            stringBuilder.append(inputCharacters.charAt(random.nextInt(inputCharacters.length())));
+        }
+        return stringBuilder.toString();
     }
 }
